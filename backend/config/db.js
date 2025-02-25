f2a0efa6056c323f2ddadbee53667aa300799249
Jiyoung <jybagnall@gmail.com>
2025-02-26 08:03:15 +0900

    modified db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,15 +2,19 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: isProduction ? "mysql.railway.internal" : process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: isProduction ? 3306 : process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  connectTimeout: 20000,
+  acquireTimeout: 20000,
   ssl:
     process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : undefined,
 });
